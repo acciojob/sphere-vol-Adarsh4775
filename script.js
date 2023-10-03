49f13278f99a6ce4c107951607e43159dec51229
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-03 18:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function volume_sphere() {
     //Write your code here
-  
+  // Define the volume_sphere function
+function volume_sphere() {
+    // Get the value of the Radius field
+    const radius = parseFloat(document.getElementById("Radius").value);
+
+    // Check if the input is a valid number
+    if (isNaN(radius)) {
+        // If not a valid number, display an error message
+        document.getElementById("volume").value = "Please enter a valid radius.";
+    } else {
+        // Calculate the volume of the sphere
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+        // Display the calculated volume in the volume field
+        document.getElementById("volume").value = volume.toFixed(2); // Limit to 2 decimal places
+    }
+}
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;

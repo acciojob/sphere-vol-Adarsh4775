29cc3067437ecfa57b10cbca9ae64353ea1e6275
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-23 19:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let caluCulate = document.getElementById("submit");
 caluCulate.addEventListener("click" , volume_sphere);
 
 function volume_sphere(){
-	
+	event.preventDefault();
 	let radius = document.getElementById("radius");
 	let volume = document.getElementById("volume");
 
@@ -14,6 +14,6 @@ function volume_sphere(){
 
 	let volValue = (4/3) * Math.PI * Math.pow(rValue,3);
 
-	 volume.value = volValue.toFixed(2);
+	 volume.value = volValue;
 	
 }

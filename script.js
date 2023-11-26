1809c350351e36264589193dc225c6d69702b682
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-26 18:06:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let calculate = document.getElementById("submit");
+document.addEventListener("DOMContentLoaded", function() {
+    let calculate = document.getElementById("submit");
 
-calculate.addEventListener("click", volume_sphere);
+    calculate.addEventListener("click", volume_sphere);
 
-function volume_sphere(event) {
-    event.preventDefault();
-    let radius = document.getElementById("radius");
-    let volume = document.getElementById("volume");
+    function volume_sphere(event) {
+        event.preventDefault();
+        let radius = document.getElementById("radius");
+        let volume = document.getElementById("volume");
 
-    let rValue = parseFloat(radius.value);
+        let rValue = parseFloat(radius.value);
 
-    if (!isNaN(rValue)) {
-        let volValue = (4/3) * Math.PI * Math.pow(rValue, 3);
-        volume.value = volValue.toFixed(4);
-    } else {
-        volume.value = "NaN"; 
+        if (!isNaN(rValue)) {
+            let volValue = (4/3) * Math.PI * Math.pow(rValue, 3);
+            volume.value = volValue.toFixed(4);
+        } else {
+            volume.value = "NaN"; 
+        }
     }
-}
+});
